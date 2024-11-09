be8b482644b85e600c0f53c8411c6eab58f5dbbf
Rick Umali <rickumali@gmail.com>
2024-11-08 21:34:16 -0500

    Adds stand-alone scikit-learn examples
    This was work from October 7 2024. These two programs are
    from the first part of Library_Comparison.ipynb notebook.

---
+++ lesson2.1-cancer-with-times.py
@@ -0,0 +1,52 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import train_test_split
+
+import time
+start_time = time.perf_counter()
+
+cancer = load_breast_cancer()
+X_scaled = StandardScaler().fit_transform(cancer.data)
+print("Original data (rows, features):", X_scaled.shape)
+
+# TIME
+# Generating to polynomial features is not that time consuming
+poly = PolynomialFeatures(2)
+X_poly = poly.fit_transform(X_scaled)
+print("All polynomial features (order 2):", X_poly.shape)
+
+rfc_start_time = time.perf_counter()
+# TIME
+# A fairly generic random forest
+rfc = RandomForestClassifier(max_depth=7, n_estimators=10, random_state=1)
+
+# Do some work to pick the optimal number of features
+# "Recursive feature elimination using cross-validation"
+rfecv = RFECV(estimator=rfc, cv=5, n_jobs=-1)
+X_poly_top = rfecv.fit_transform(X_poly, cancer.target)
+
+# The "top" features selected for the model
+rfc_end_time = time.perf_counter()
+total_rfc_time = rfc_end_time - rfc_start_time
+print("Best polynomial features", X_poly_top.shape)
+print(f"RandomForestClassifier time: {total_rfc_time:.4f} seconds")
+
+# TIME
+# Do a train/test split on the "poly_top" features
+X_train, X_test, y_train, y_test = train_test_split(
+    X_poly_top, cancer.target, random_state=42)
+
+# Train the selected RFC model
+rfc = RandomForestClassifier(max_depth=7, n_estimators=10, random_state=1)
+print("Test accuracy:", rfc.fit(X_train, y_train).score(X_test, y_test))
+
+end_time = time.perf_counter()
+total_elapsed_time = end_time - start_time
+print(f"Total elapsed time: {total_elapsed_time:.4f} seconds")
+
